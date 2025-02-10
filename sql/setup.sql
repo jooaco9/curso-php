1ae45b119c2939c14735314a924f28c56077dffa
jooaco9 <joaquin.corbo@gmail.com>
2025-02-10 20:14:04 -0300

    Empezando con la base de datos

---
+++ sql/setup.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE contacts_app;
+
+USE contacts_app;
+
+CREATE TABLE contacts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  phone_number VARCHAR(255)
+);
+
+INSERT INTO contacts (name, phone_number) VALUES ("Joaquin", "099797372");
